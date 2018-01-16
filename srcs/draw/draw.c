d900ce09dff5e8236249f0e70d2be514485a0851
Adrian LEAU <adleau@e2r10p11.42.fr>
2018-01-16 17:51:22 +0100

    dfghs

--- srcs/draw/draw.c
+++ srcs/draw/draw.c
@@ -6,7 +6,7 @@
 /*   By: adleau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 14:31:00 by adleau            #+#    #+#             */
-/*   Updated: 2018/01/16 14:38:53 by adleau           ###   ########.fr       */
+/*   Updated: 2018/01/16 17:50:54 by adleau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,23 @@
 
 void		draw_menu(t_engine *eng)
 {
+	Uint32 rmask, gmask, bmask, amask;
 
+	/* SDL interprets each pixel as a 32-bit number, so our masks must depend
+	   on the endianness (byte order) of the machine */
+	#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+	#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+	#endif
+
+	eng->mainwindow->menu = SDL_CreateRGBSurface(0, width, height, 32, rmask, gmask, bmask, amask);
 }
 
 /* draw function
