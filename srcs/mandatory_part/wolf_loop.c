27cf2db8ac912d85e93423575e3308d43406acfb
Adrian LEAU <adleau@e2r10p17.42.fr>
2018-02-16 15:12:14 +0100

    debug movements, debug null orientation

--- srcs/mandatory_part/wolf_loop.c
+++ srcs/mandatory_part/wolf_loop.c
@@ -6,7 +6,7 @@
 /*   By: adleau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 08:27:33 by adleau            #+#    #+#             */
-/*   Updated: 2018/02/16 09:02:02 by adleau           ###   ########.fr       */
+/*   Updated: 2018/02/16 13:42:02 by adleau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,53 @@ void		keyup_events_w3d(t_wolf *wolf)
 		free_sdl_wrapper(wolf->wrap);
 		exit(0);
 	}
-	if (EVENT_PTR.key.keysym.sym == SDLK_LEFT)
+	if (EVENT_PTR.key.keysym.sym == SDLK_RIGHT)
 	{
-		wolf->map.cam.orientation += 10;
-		init_w3dcam(&(wolf->map.cam));
-//		printf("%f\n", wolf->map.cam.orientation);
+		wolf->map.cam.orientation += 5;
 		if (wolf->map.cam.orientation >= 360)
 			wolf->map.cam.orientation -= 360;
-			w3d_draw(wolf);
+//		w3d_draw(wolf);
 	}
+	if (EVENT_PTR.key.keysym.sym == SDLK_LEFT)
+	{
+		wolf->map.cam.orientation -= 5;
+		if (wolf->map.cam.orientation < 0)
+			wolf->map.cam.orientation = 360 + wolf->map.cam.orientation;
+//		w3d_draw(wolf);
+	}
+	if (EVENT_PTR.key.keysym.sym == SDLK_UP)
+	{
+		int tmpx;
+		int tmpy;
+		tmpx = wolf->map.cam.player.x;
+		tmpy = wolf->map.cam.player.y;
+		wolf->map.cam.player.x += (wolf->map.cam.direction.x * 10 * (cos(wolf->map.cam.angle * M_PI / 180)));
+		wolf->map.cam.player.y += (wolf->map.cam.direction.y * 10 * sin(wolf->map.cam.angle * M_PI / 180));
+		if (wolf->map.cam.player.x >= CELL || wolf->map.cam.player.x < 0)
+		{
+			if (wolf->map.map[wolf->map.pos.y][wolf->map.pos.x + (wolf->map.cam.direction.x)] == '1')
+				wolf->map.cam.player.x = tmpx;
+			else
+			{
+				wolf->map.pos.x += wolf->map.cam.direction.x;
+				wolf->map.cam.player.x = (wolf->map.cam.direction.x > 0) ? 0 : CELL;
+			}
+		}
+		if (wolf->map.cam.player.y >= CELL  || wolf->map.cam.player.y < 0)
+		{
+			if (wolf->map.map[wolf->map.pos.y + wolf->map.cam.direction.y][wolf->map.pos.x] == '1')
+				wolf->map.cam.player.y = tmpy;
+			else
+			{
+				wolf->map.pos.y += wolf->map.cam.direction.y;
+				wolf->map.cam.player.y = (wolf->map.cam.direction.y > 0) ? 0 : CELL;
+			}
+		}
+		printf("dyude %d | %d %d %f %f\n", wolf->map.cam.direction.y, wolf->map.cam.player.y, wolf->map.cam.player.x, (wolf->map.cam.direction.x * sin(wolf->map.cam.angle * M_PI / 180)),wolf->map.cam.angle);
+	}
+/*	if (EVENT_PTR.key.keysym.sym == SDLK_DOWN)
+	;
+*/
 }
 
 /* wolf_loop function
@@ -51,7 +89,7 @@ void		wolf_loop(t_wolf *wolf)
 			w3d_draw(wolf);
 			if (EVENT_PTR.type == SDL_QUIT)
 				exit(0);
-			else if (EVENT_PTR.type == SDL_KEYUP)
+			else if (EVENT_PTR.type == SDL_KEYDOWN)
 				keyup_events_w3d(wolf);
 /*			else if (EVENT_PTR.type == SDL_MOUSEBUTTONUP)
 			if ((ret = mouseup_events(eng)))
